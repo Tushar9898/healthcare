d232bfb04ddda466ce6e0697660d73a3e47c88d6
Tushar9898 <tushar978777@gmail.com>
2025-08-14 00:28:31 +0530

    Commit

--- voice_of_the_patient.py
+++ voice_of_the_patient.py
@@ -1,6 +1,6 @@
 # if you dont use pipenv uncomment the following:
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 
 #Step1: Setup Audio recorder (ffmpeg & portaudio)
 # ffmpeg, portaudio, pyaudio
@@ -49,7 +49,7 @@ audio_filepath="patient_voice_test_for_patient.mp3"
 import os
 from groq import Groq
 
-GROQ_API_KEY=os.getenv("ELEVENLABS_API_KEY")
+GROQ_API_KEY=os.getenv("GROQ_API_KEY")
 stt_model="whisper-large-v3"
 
 def transcribe_with_groq(stt_model, audio_filepath, GROQ_API_KEY):
